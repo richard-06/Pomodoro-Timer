729087f29192649d846db94f27e0bd2f2f9bb238
richard-06 <richardroshan0110@gmail.com>
2025-03-27 14:24:58 +0000

    initialzing vite gh-page

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
+   base: '/pomodoro-timer/', 
+   server:{
     port:3200,
     open:true
   },
